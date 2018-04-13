c5f9313214f5df58f945403a18fbdf27428262ab
Dmitry Stepanov <DmStepanov@hotmail.com>
2018-04-13 17:56:16 +0500

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent }      from './dashboard/dashboard.component';
 import { PagesComponent }      from './pages/pages.component';
+import { UploadsComponent }      from './uploads/uploads.component';
 
 
 const routes: Routes = [
 	{ path: '', redirectTo: '/dashboard', pathMatch: 'full' },
 	{ path: 'dashboard', component: DashboardComponent },
-	{ path: 'pages', component: PagesComponent }
+	{ path: 'pages', component: PagesComponent },
+	{ path: 'uploads', component: PagesComponent }
 ];
 
 @NgModule({
