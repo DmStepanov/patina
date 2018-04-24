d317f9e586761d8922e5cb3efb01eac1186ee9dd
Dmitry Stepanov <DmStepanov@hotmail.com>
2018-04-24 10:26:36 +0500

    e2e tests

--- src/app/index/index.component.spec.ts
+++ src/app/index/index.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientModule } from '@angular/common/http';
 import { IndexComponent } from './index.component';
 
 describe('IndexComponent', () => {
@@ -8,7 +8,12 @@ describe('IndexComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ IndexComponent ]
+      declarations: [ 
+        IndexComponent 
+      ],
+      imports :[ 
+        HttpClientModule
+      ]
     })
     .compileComponents();
   }));
